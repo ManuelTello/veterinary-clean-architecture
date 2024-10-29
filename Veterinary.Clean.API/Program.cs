02331f644874e466b243ded78c4ed3eadbfb98ac
Manuel Tello Siri <manuelTello.business@gmail.com>
2024-10-29 11:58:21 -0700

    Add project files.

---
+++ Veterinary.Clean.API/Program.cs
@@ -0,0 +1,25 @@
+using Veterinary.Clean.Infrastructure.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDatabaseContext>();
+builder.Services.AddMediatR(options =>
+{
+    options.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
---
+++ Veterinary.Clean.Infrastructure/Data/ApplicationDatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Veterinary.Clean.Infrastructure.Data
+{
+    public class ApplicationDatabaseContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            base.OnConfiguring(options);
+            const string connectionString = "";
+            options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 30)));
+        }
+    }
+}
